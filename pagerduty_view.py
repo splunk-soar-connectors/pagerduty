289afa2672e3e7b527459df831564307d5dcf6c6
kzuchowska <kzuchowska@splunk.com>
2024-09-25 11:29:58 +0200

    PAPP-34728 update dependencies and prew-commit

--- pagerduty_view.py
+++ pagerduty_view.py
@@ -22,82 +22,82 @@ def get_ctx_result(result):
     summary = result.get_summary()
     data = result.get_data()
 
-    ctx_result['param'] = param
+    ctx_result["param"] = param
 
-    if (data):
-        ctx_result['policies'] = data
+    if data:
+        ctx_result["policies"] = data
 
-    if (summary):
-        ctx_result['summary'] = summary
+    if summary:
+        ctx_result["summary"] = summary
 
     return ctx_result
 
 
 def display_oncall_user(provides, all_app_runs, context):
 
-    context['results'] = results = []
+    context["results"] = results = []
     for summary, action_results in all_app_runs:
         for result in action_results:
 
             ctx_result = get_ctx_result(result)
-            if (not ctx_result):
+            if not ctx_result:
                 continue
             results.append(ctx_result)
     # print context
-    return 'display_oncall_user.html'
+    return "display_oncall_user.html"
 
 
 def display_user_info(provides, all_app_runs, context):
 
-    context['results'] = results = []
+    context["results"] = results = []
     for summary, action_results in all_app_runs:
         for result in action_results:
 
             ctx_result = get_ctx_result(result)
-            if (not ctx_result):
+            if not ctx_result:
                 continue
             results.append(ctx_result)
     # print context
-    return 'display_user_info.html'
+    return "display_user_info.html"
 
 
 def display_escalations(provides, all_app_runs, context):
 
-    context['results'] = results = []
+    context["results"] = results = []
     for summary, action_results in all_app_runs:
         for result in action_results:
 
             ctx_result = get_ctx_result(result)
-            if (not ctx_result):
+            if not ctx_result:
                 continue
             results.append(ctx_result)
     # print context
-    return 'display_escalations.html'
+    return "display_escalations.html"
 
 
 def display_users(provides, all_app_runs, context):
 
-    context['results'] = results = []
+    context["results"] = results = []
     for summary, action_results in all_app_runs:
         for result in action_results:
 
             ctx_result = get_ctx_result(result)
-            if (not ctx_result):
+            if not ctx_result:
                 continue
             results.append(ctx_result)
     # print context
-    return 'display_users.html'
+    return "display_users.html"
 
 
 def display_services(provides, all_app_runs, context):
 
-    context['results'] = results = []
+    context["results"] = results = []
     for summary, action_results in all_app_runs:
         for result in action_results:
 
             ctx_result = get_ctx_result(result)
-            if (not ctx_result):
+            if not ctx_result:
                 continue
             results.append(ctx_result)
     # print context
-    return 'display_services.html'
+    return "display_services.html"
